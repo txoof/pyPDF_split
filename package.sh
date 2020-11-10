62a45260652c36a3d97a51270b6813ca4e92dba9
txoof <aaron.ciuffo@gmail.com>
2020-11-10 13:28:41 +0100

    refresh pkg

--- package.sh
+++ package.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-pkgname=pypdfsplit.pkg
-pycodesign.py pycodesign.ini 
+source_path=./pyPDF_split
+publish_pkg=pypdfsplit.pkg
 
-git commit -m "repackage, sign, notarize $pkgname"
+version_number=`grep version $source_path/constants.py | sed -nE  's/^version[ ]+=[ ]+(.*)/\1/p' | tr -d \'\"`
+
+if [ -z "$1" ]; then
+  echo current version number: v$version_number
+  echo pkg build, sign notarize, tag and push release to github
+  echo usage:
+  echo $0 \"release comment\"
+  exit 0
+fi
+
+tag="v$version_number"
+
+pycodesign.py -O $version_number pycodesign.ini
+
+if [ $? -ne 0 ]; then
+  echo signing failed
+  exit
+fi
+
+git tag -a "$tag" -m "$1"
+git commit -m "update pkg distribution $1" $publish_pkg
+git push origin $tag
+# git commit -m "repackage, sign, notarize $pkgname"
--- pyPDF_split/constants.py
+++ pyPDF_split/constants.py
@@ -1,5 +1,3 @@
-contact="Aaron Ciuffo -- aaron.ciuffo@gmail.com"
-version="0.1.1"
+contact = "Aaron Ciuffo -- aaron.ciuffo@gmail.com"
+version = "0.1.1"
 
-if __name__ == "__main__":
-  print(version)
